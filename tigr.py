69f2e49f81c618b59d525744b3303170e45732bf
Void <bogdanpogorelov2008@gmail.com>
2025-01-31 18:11:21 +0300

    Updated the update method

--- tigr.py
+++ tigr.py
@@ -4,28 +4,30 @@ import time
 class Field:
     def __init__(self, size):
         self.size = size
-        self.grid = []
-        for _ in range(size):
-            self.grid.append([None] * size)
+
         self.tiger = Tiger()
         self.rabbit1 = Rabbit()
         self.rabbit2 = Rabbit()
-        self.grid[self.tiger.y][self.tiger.x] = self.tiger
-        self.grid[self.rabbit1.y][self.rabbit1.x] = self.rabbit1
-        self.grid[self.rabbit2.y][self.rabbit2.x] = self.rabbit2
 
     def display(self):
-        for i, row in enumerate(self.grid):
+        self.tiger.update([self.rabbit1, self.rabbit2])
+        for y in range(self.size):
             display_row = ""
-            for cell in row:
-                if cell is None:
-                    display_row += ". "
-                elif isinstance(cell, Rabbit):
+            for x in range(self.size):
+                if self.rabbit1.x == x and self.rabbit1.y == y:
                     display_row += "З "
-                else:
+                elif self.rabbit2.x == x and self.rabbit2.y == y:
+                    display_row += "З "
+                elif self.tiger.x == x and self.tiger.y == y:
                     display_row += "Т "
-                    cell.update([self.grid[self.rabbit1.y][self.rabbit1.x], self.grid[self.rabbit2.y][self.rabbit2.x]])
+                else:
+                    display_row += ". "
+
             print(display_row)
+        print(self.tiger)
+        print(self.rabbit1)
+        print(self.rabbit2)
+
 
 class Tiger:
     def __init__(self):
@@ -33,10 +35,22 @@ class Tiger:
         self.x = 0
         self.y = 0
 
+    def __str__(self):
+        return f"Тигр x:{self.x} y:{self.y}"
+
+
     def update(self, rabbits):
         if self.state == "Выследить добычу":
             self.x += random.randint(-1, 1)
+            if self.x < 0:
+                self.x = 0
+            if self.x > 4:
+                self.x = 4
             self.y += random.randint(-1, 1)
+            if self.y < 0:
+                self.y = 0
+            if self.y > 4:
+                self.y = 4
             for rabbit in rabbits:
                 if self.x - rabbit.x == 1 and self.y - rabbit.y == 1:
                     self.state = "Атаковать добычу"
@@ -50,9 +64,15 @@ class Tiger:
 
     def attack(self, rabbits):
         for rabbit in rabbits:
-            if self.x - rabbit.x == 1 and self.y - rabbit.y == 1:
+            if self.x - rabbit.x <= 1 and self.y - rabbit.y <= 1:
                 self.state = "Бежать домой"
                 del rabbit
+
+    def run_in_home(self):
+        self.x = 0
+        self.y = 0
+
+
 
 class Rabbit:
     def __init__(self):
@@ -62,8 +82,12 @@ class Rabbit:
             if self.x != 0 or self.y != 0:
                 break
 
+    def __str__(self):
+        return f"Заяц x:{self.x} y:{self.y}"
+
+
 field = Field(5)
 
-while True:
+while field.tiger.state != "Бежать домой":
     field.display()
     time.sleep(1)
